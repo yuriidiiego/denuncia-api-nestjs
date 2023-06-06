1a2e9723dc4d22a067322da9c3d665cd51daa141
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-06 17:05:54 -0300

    refactor: ♻️ refactor AppModule
    Refactor the AppModule by adding new imports and removing unnecessary imports. In the old commit, the AppModule only imported the AppController and AppService. In the new commit, the AppModule imports additional modules such as DenunciaModule, PrismaModule, MapQuestModule, ConfigModule, and HttpModule. These new imports provide the necessary dependencies and configurations for the application. The controllers and providers arrays have been emptied as there are no specific controllers or providers registered directly in the AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DenunciaModule } from './denuncia/denuncia.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { MapQuestModule } from './external/mapquest/mapquest.module';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    HttpModule,
+    DenunciaModule,
+    PrismaModule,
+    MapQuestModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
