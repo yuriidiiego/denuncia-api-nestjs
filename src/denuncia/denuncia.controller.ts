ad3513b4761ff7365a7fef572d8c17dc78832af7
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-12 18:47:21 -0300

    feat: ✨ update DenunciaController
    - Added additional routes to the DenunciaController:
      - GET /denuncias: Retrieves all denuncias.
      - GET /denuncias/:id: Retrieves a specific denuncia by ID.
      - DELETE /denuncias/:id: Deletes a denuncia by ID.
      - PATCH /denuncias/:id: Updates a denuncia by ID.
    - Updated the create route to include the appropriate response type.

--- src/denuncia/denuncia.controller.ts
+++ src/denuncia/denuncia.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { DenunciaService } from './denuncia.service';
 import { DenunciaRequest } from './payload/request/create-denuncia.request';
+import { UpdateDenunciaRequest } from './payload/request/update-denuncia.request';
+import { DenunciaResponse } from './payload/response/create-denuncia.response';
 
 @Controller('denuncias')
 export class DenunciaController {
@@ -10,5 +21,29 @@ export class DenunciaController {
   @HttpCode(201)
   async create(@Body() denunciaRequest: DenunciaRequest) {
     return this.denunciaService.createDenuncia(denunciaRequest);
+  }
+
+  @Get()
+  async getDenuncias() {
+    return this.denunciaService.getDenuncias();
+  }
+
+  @Get(':id')
+  async getDenuncia(@Param('id') id: number) {
+    return this.denunciaService.getDenunciaById(id);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async deleteDenuncia(@Param('id') id: number) {
+    return this.denunciaService.deleteDenuncia(id);
+  }
+
+  @Patch(':id')
+  async updateDenuncia(
+    @Param('id') id: number,
+    @Body() updateRequest: UpdateDenunciaRequest,
+  ): Promise<DenunciaResponse> {
+    return this.denunciaService.updateDenuncia(id, updateRequest);
   }
 }
