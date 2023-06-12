03f75eda4e9323898c6955c8002e4fe7ad0fc8b1
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-12 18:45:31 -0300

    refactor: ♻️ update DenunciaModule
    - Updated the DenunciaModule to include the DenunciaMapper as a provider.

--- src/denuncia/denuncia.module.ts
+++ src/denuncia/denuncia.module.ts
@@ -3,10 +3,11 @@ import { DenunciaController } from './denuncia.controller';
 import { DenunciaService } from './denuncia.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { MapQuestModule } from '../external/mapquest/mapquest.module';
+import { DenunciaMapper } from './denuncia.mapper';
 
 @Module({
   imports: [PrismaModule, MapQuestModule],
   controllers: [DenunciaController],
-  providers: [DenunciaService],
+  providers: [DenunciaService, DenunciaMapper],
 })
 export class DenunciaModule {}
