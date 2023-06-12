2552544dac6c4052614b38abe32155d2b68964e5
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-12 18:42:43 -0300

    feat: ✨ add MapQuestLocation interface
    - Added the MapQuestLocation interface to define the structure of location data returned by the MapQuest API.

---
+++ src/external/mapquest/mapquest-location.interface.ts
@@ -0,0 +1,13 @@
+interface MapQuestLocation {
+    results: {
+      locations: {
+        postalCode?: string;
+        street?: string;
+        adminArea6?: string;
+        adminArea5?: string;
+        adminArea3?: string;
+        adminArea1?: string;
+      }[];
+    }[];
+  }
+  
