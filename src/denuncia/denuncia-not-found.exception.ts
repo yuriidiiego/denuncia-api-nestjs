b14bc2a41193656425095e239dce1acb804b53e0
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-15 15:57:59 -0300

    refactor: ♻️ refactor DenunciaNotFoundException
    Refactor the DenunciaNotFoundException class by updating the exception message. In the old commit, the exception message included the ID of the denúncia in Portuguese and a descriptive text. In the new commit, the message is simplified to convey that the denuncia with the specified ID was not found. This change improves consistency and ensures a more straightforward and understandable exception message.

--- src/denuncia/denuncia-not-found.exception.ts
+++ src/denuncia/denuncia-not-found.exception.ts
@@ -2,7 +2,6 @@ import { NotFoundException } from '@nestjs/common';
 
 export class DenunciaNotFoundException extends NotFoundException {
   constructor(id: number) {
-    super(`Denúncia com ID ${id} não encontrada.`);
+    super(`Denuncia ${id} not found`);
   }
 }
-    
