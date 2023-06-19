bbbf42afdb1b142857609dcb6599bba1a3696eb6
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-19 18:09:35 -0300

    refactor: ♻️ update DenunciaController with Swagger API annotations
    The DenunciaController was updated to include Swagger API annotations for better documentation and generation of API documentation.
    
    Changes:
    - Added `ApiTags` to categorize the controller under "Denúncias".
    - Added `ApiOperation` to provide a summary and operation ID for each endpoint.
    - Added `ApiResponse` to describe the successful response for the getDenuncias endpoint.
    - Added `ApiParam` to document the parameter for the getDenuncia, deleteDenuncia, and updateDenuncia endpoints.
    - Added `ApiBody` to specify the request body type for the updateDenuncia endpoint.
    - Added `ApiCreatedResponse`, `ApiOkResponse`, `ApiNotFoundResponse`, `ApiBadRequestResponse`, and `ApiNoContentResponse` annotations to the respective endpoints.
    - Updated the return types of the create, getDenuncias, deleteDenuncia, and updateDenuncia endpoints to specify the response types.

--- src/denuncia/denuncia.controller.ts
+++ src/denuncia/denuncia.controller.ts
@@ -1,49 +1,57 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiBody, ApiCreatedResponse, ApiOkResponse, ApiNotFoundResponse, ApiBadRequestResponse, ApiNoContentResponse } from '@nestjs/swagger';
 import { DenunciaService } from './denuncia.service';
 import { DenunciaRequest } from './payload/request/create-denuncia.request';
 import { UpdateDenunciaRequest } from './payload/request/update-denuncia.request';
 import { DenunciaResponse } from './payload/response/create-denuncia.response';
 
+@ApiTags('Denúncias')
 @Controller('denuncias')
 export class DenunciaController {
   constructor(private readonly denunciaService: DenunciaService) {}
 
   @Post()
   @HttpCode(201)
-  async create(@Body() denunciaRequest: DenunciaRequest) {
+  @ApiOperation({ summary: 'Criar uma denúncia', operationId: 'createDenuncia' })
+  @ApiCreatedResponse({ description: 'Denúncia criada com sucesso', type: DenunciaResponse })
+  @ApiBadRequestResponse({ description: 'Dados inválidos no corpo da requisição', type: ApiBadRequestResponse })
+  async create(@Body() denunciaRequest: DenunciaRequest): Promise<DenunciaResponse> {
     return this.denunciaService.createDenuncia(denunciaRequest);
   }
 
   @Get()
-  async getDenuncias() {
+  @ApiOperation({ summary: 'Obter todas as denúncias', operationId: 'getDenuncias' })
+  @ApiResponse({ status: 200, description: 'Lista de denúncias recuperada com sucesso', type: DenunciaResponse, isArray: true })
+  async getDenuncias(): Promise<DenunciaResponse[]> {
     return this.denunciaService.getDenuncias();
   }
 
   @Get(':id')
-  async getDenuncia(@Param('id') id: number) {
+  @ApiOperation({ summary: 'Obter uma denúncia pelo ID', operationId: 'getDenuncia' })
+  @ApiParam({ name: 'id', description: 'ID da denúncia', type: 'number' })
+  @ApiOkResponse({ description: 'Denúncia recuperada com sucesso', type: DenunciaResponse, status: 200 })
+  @ApiNotFoundResponse({ description: 'Denúncia não encontrada', status: 404 })
+  async getDenuncia(@Param('id') id: number): Promise<DenunciaResponse> {
     return this.denunciaService.getDenunciaById(id);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Excluir uma denúncia pelo ID', operationId: 'deleteDenuncia' })
+  @ApiParam({ name: 'id', description: 'ID da denúncia', type: 'number' })
+  @ApiNoContentResponse({ description: 'Denúncia excluída com sucesso', status: 204 })
+  @ApiNotFoundResponse({ description: 'Denúncia não encontrada', status: 404 })
   @HttpCode(204)
-  async deleteDenuncia(@Param('id') id: number) {
-    return this.denunciaService.deleteDenuncia(id);
+  async deleteDenuncia(@Param('id') id: number): Promise<void> {
+    await this.denunciaService.deleteDenuncia(id);
   }
 
   @Patch(':id')
-  async updateDenuncia(
-    @Param('id') id: number,
-    @Body() updateRequest: UpdateDenunciaRequest,
-  ): Promise<DenunciaResponse> {
+  @ApiOperation({ summary: 'Atualizar uma denúncia pelo ID', operationId: 'updateDenuncia' })
+  @ApiParam({ name: 'id', description: 'ID da denúncia', type: 'number' })
+  @ApiBody({ type: UpdateDenunciaRequest })
+  @ApiOkResponse({ description: 'Denúncia atualizada com sucesso', type: DenunciaResponse })
+  @ApiNotFoundResponse({ description: 'Denúncia não encontrada', status: 404 })
+  async updateDenuncia(@Param('id') id: number, @Body() updateRequest: UpdateDenunciaRequest): Promise<DenunciaResponse> {
     return this.denunciaService.updateDenuncia(id, updateRequest);
   }
 }
