f5369d11ca59f7a7b7600fcc52544c37aee1ae41
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-19 17:59:38 -0300

    refactor: 🚀 update bootstrap function in main.ts
    Updated the bootstrap function in the main.ts file to enhance the Nest.js application with additional features.
    
    The following changes were made:
    - Added a ValidationPipe to enable request payload validation with whitelisting enabled.
    - Added an HttpExceptionFilter to handle and format HTTP exceptions globally.
    - Integrated Swagger documentation using the setupSwagger function.
    
    This update improves the application by enabling request payload validation, standardized exception handling, and API documentation generation with Swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { setupSwagger } from '../swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
+  setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
