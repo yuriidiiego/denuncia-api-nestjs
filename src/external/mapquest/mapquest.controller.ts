c50a37eb4d4523ef89a7bafcd388a4060f32fd86
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-19 18:08:29 -0300

    refactor: ♻️ update MapQuestController with Swagger API annotations
    The MapQuestController was updated to include Swagger API annotations for better documentation and generation of API documentation.
    
    Changes:
    - Added `ApiTags` to categorize the controller under "Endereços".
    - Added `ApiOperation` to provide a summary, description, and operation ID for the getAddressFromCoordinates endpoint.
    - Added `ApiOkResponse` to describe the successful response with a reference to the Endereco schema.
    - Added `ApiInternalServerErrorResponse` to describe the error response for failure to retrieve the address.
    - Added `ApiQuery` to document the query parameters for latitude and longitude.
    - Added `ApiTags`, `ApiOperation`, `ApiOkResponse`, `ApiInternalServerErrorResponse`, and `ApiQuery` annotations to the getAddressFromCoordinates endpoint.

--- src/external/mapquest/mapquest.controller.ts
+++ src/external/mapquest/mapquest.controller.ts
@@ -1,11 +1,47 @@
+import {
+  ApiTags,
+  ApiOperation,
+  ApiQuery,
+  ApiOkResponse,
+  ApiInternalServerErrorResponse,
+} from '@nestjs/swagger';
 import { Controller, Get, Query } from '@nestjs/common';
-import { MapQuestService } from './mapquest.service';
 import { Endereco } from '@prisma/client';
+import { MapQuestService } from './mapquest.service';
 
+@ApiTags('Endereços')
 @Controller('endereco')
 export class MapQuestController {
   constructor(private readonly mapQuestService: MapQuestService) {}
 
+  @ApiOperation({
+    summary: 'Obter endereço por coordenadas',
+    description: 'Retorna o endereço pelas coordenadas',
+    operationId: 'getAddressFromCoordinates',
+  })
+  @ApiOkResponse({
+    description: 'Endereço recuperado com sucesso',
+    status: 200,
+    schema: {
+      $ref: '#/components/schemas/Endereco',
+    },
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Falha ao recuperar o endereço da API do MapQuest.',
+    status: 500,
+  })
+  @ApiQuery({
+    name: 'lat',
+    type: Number,
+    description: 'Latitude das coordenadas',
+    example: 51.507351,
+  })
+  @ApiQuery({
+    name: 'long',
+    type: Number,
+    description: 'Longitude das coordenadas',
+    example: -0.127758,
+  })
   @Get()
   async getAddressFromCoordinates(
     @Query('lat') lat: number,
