1bdbbb5ce0edaeaa41c1333866b979b52bbdcca9
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-19 18:10:10 -0300

    refactor: ♻️ update DenunciaNotFoundException message
    The DenunciaNotFoundException message was updated to use Portuguese language.
    
    Changes:
    - Updated the exception message from "Denuncia ${id} not found" to "Denúncia ${id} não encontrada".
    
    This change improves the consistency and clarity of the exception message.

--- src/denuncia/denuncia-not-found.exception.ts
+++ src/denuncia/denuncia-not-found.exception.ts
@@ -2,6 +2,6 @@ import { NotFoundException } from '@nestjs/common';
 
 export class DenunciaNotFoundException extends NotFoundException {
   constructor(id: number) {
-    super(`Denuncia ${id} not found`);
+    super(`Denúncia ${id} não encontrada`);
   }
 }
