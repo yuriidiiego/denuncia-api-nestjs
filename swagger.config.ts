97808f76724f961fa535b1ba29631ff417bb91bf
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-19 17:53:59 -0300

    docs: 📝 add Swagger setup function
    Added a new function named "setupSwagger" that sets up Swagger documentation for the NestJS application. This function creates a Swagger document with custom configuration options, including the API title, description, contact information, license, and version. It then sets up the Swagger module in the application.
    
    The function "setupSwagger" was added to the codebase, enhancing the application with Swagger documentation capabilities.

---
+++ swagger.config.ts
@@ -0,0 +1,20 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export function setupSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('Denuncia API')
+    .setDescription(
+      'Este projeto consiste em um sistema de denúncias, desenvolvido utilizando o framework NestJS. O objetivo principal é fornecer uma plataforma para que os usuários possam enviar denúncias sobre determinados incidentes.',
+    )
+    .setContact(
+      'Yuri Nascimento',
+      'https://github.com/yuriidiiego',
+      'yuriidiiego@gmail.com',
+    )
+    .setLicense('Apache 2.0', 'https://www.apache.org/licenses/LICENSE-2.0')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
