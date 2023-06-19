692241e771dfe86687a66c14dc876ba4b809c6e0
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-19 18:07:35 -0300

    refactor: ♻️ update response class with Swagger API annotations
    The response class `DenunciaResponse` was updated to include Swagger API annotations for better documentation and generation of API documentation.
    
    Changes:
    - Added `ApiProperty` annotations to provide example values and descriptions for each property.

--- src/denuncia/payload/response/create-denuncia.response.ts
+++ src/denuncia/payload/response/create-denuncia.response.ts
@@ -1,9 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Denunciante, Endereco } from '@prisma/client';
 
 export class DenunciaResponse {
+  @ApiProperty({ example: 1, description: 'O ID da denúncia' })
   id: number;
+
+  @ApiProperty({
+    example: 'Título da denúncia',
+    description: 'O título da denúncia',
+  })
   titulo: string;
+
+  @ApiProperty({
+    example: 'Descrição da denúncia',
+    description: 'A descrição da denúncia',
+  })
   descricao: string;
+
+  @ApiProperty({
+    description: 'Informações sobre o denunciante',
+    example: {
+      nome: 'Nome do denunciante',
+      cpf: '123.456.789-00',
+    },
+  })
   denunciante: Denunciante;
+
+  @ApiProperty({
+    description: 'Informações sobre o endereço',
+    example: {
+      cep: '12345678',
+      rua: 'Rua teste',
+      bairro: 'Bairro teste',
+      cidade: 'Cidade teste',
+      estado: 'Estado teste',
+      pais: 'País teste',
+    },
+  })
   endereco: Endereco;
 }
