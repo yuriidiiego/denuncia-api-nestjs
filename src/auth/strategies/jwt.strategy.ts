e75eff7e8feba16df08b1ce0f0808296694a58fa
Yuri Nascimento <yuriidiiego@gmail.com>
2023-06-26 16:32:41 -0300

    feat:🔒 Add JwtStrategy class for JWT authentication
    - Imported necessary dependencies from '@nestjs/common', '@nestjs/passport', and 'passport-jwt'.
    - Implemented JwtStrategy class, extending PassportStrategy and using Strategy from 'passport-jwt'.
    - Added constructor to initialize the authService and configure the JWT strategy.
    - Implemented the validate method to validate the JWT payload and retrieve the user using the authService.
    - If the user is not found, an UnauthorizedException is thrown.

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { JwtPayload } from '../jwt-payload.interface';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.authService.validateUserById(payload.userId);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+  }
+}
