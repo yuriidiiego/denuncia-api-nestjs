bdcf59be5002f326d3d50476561443ba7673fb81
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-03 20:57:32 -0300

    refactor: ♻️ refactor AuthService - Simplify login functionality and remove unused code
    - Removed the `register` and `refreshToken` methods from the `AuthService` class, as they were not used.
    - Simplified the `login` method to only handle authentication and token generation.
    - Updated the dependencies in the constructor to only include `JwtService` and `UserService`.
    - Removed unused imports and code related to `PrismaService`, `BcryptService`, and `UserMapper`.
    - Created a new `createJwtPayload` method to generate the payload for the JWT token.
    - Updated the return type of the `login` method to use the new response type `JwtLoginResponse`.
    - Renamed the `LoginResponse` type to `JwtLoginResponse` for clarity.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,101 +1,33 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { User, UserRole } from '@prisma/client';
-import { PrismaService } from '../prisma/prisma.service';
-import { InvalidCredentialsException } from './auth-exceptions';
-import { BcryptService } from './bcrypt.service';
+import { UserService } from '../user/user.service';
 import { JwtPayload } from './jwt-payload.interface';
 import { AuthCredentialsRequest } from './payload/request/auth-credentials.request';
-import { AuthRegisterRequest } from './payload/request/auth-register.request';
-import { LoginResponse } from './payload/response/auth-login.response';
-import { AuthUserResponse } from './payload/response/auth-user.response';
-import { UserMapper } from './user.mapper';
+import { JwtLoginResponse } from './payload/response/jwt-login.response';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly prismaService: PrismaService,
     private readonly jwtService: JwtService,
-    private readonly bcryptService: BcryptService,
-    private readonly userMapper: UserMapper,
+    private readonly userService: UserService,
   ) {}
 
-  async register(register: AuthRegisterRequest): Promise<AuthUserResponse> {
-    const { name, email, password } = register;
-
-    const hashedPassword = await this.bcryptService.hashPassword(password);
-
-    const userRole = await this.prismaService.role.findUnique({
-      where: { name: UserRole.USER },
-    });
-
-    if (!userRole) {
-      throw new NotFoundException(`Perfil ${UserRole.USER} não encontrado!`);
-    }
-
-    const createdUser = await this.prismaService.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-        role: { connect: { id: userRole.id } },
-      },
-    });
-
-    return this.userMapper.mapToResponse(createdUser);
-  }
-
-  async login(
-    authCredentialsRequest: AuthCredentialsRequest,
-  ): Promise<LoginResponse> {
-    const { email, password } = authCredentialsRequest;
-
-    const user = await this.validateUserCredentials(email, password);
-
-    const payload: JwtPayload = { userId: user.id };
-    const accessToken = this.jwtService.sign(payload);
-
-    return { accessToken };
-  }
-
-  async refreshToken(refreshToken: string): Promise<LoginResponse> {
-    const payload: JwtPayload = { userId: 1 };
-    const accessToken = this.jwtService.sign(payload);
-
-    return { accessToken };
-  }
-
-  async validateUserCredentials(
-    email: string,
-    password: string,
-  ): Promise<User> {
-    const user = await this.prismaService.user.findUnique({ where: { email } });
-
-    if (!user) {
-      throw new InvalidCredentialsException();
-    }
-
-    const isPasswordValid = await this.bcryptService.comparePasswords(
-      password,
-      user.password,
+  async login(credentials: AuthCredentialsRequest): Promise<JwtLoginResponse> {
+    const user = await this.userService.validateUserCredentials(
+      credentials.email,
+      credentials.password,
     );
-
-    if (!isPasswordValid) {
-      throw new InvalidCredentialsException();
-    }
-
-    return user;
+    const payload: JwtPayload = this.createJwtPayload(user.id);
+    return {
+      accessToken: this.generateAccessToken(payload),
+    };
   }
 
-  async validateUserById(userId: number): Promise<User> {
-    const user = await this.prismaService.user.findUnique({
-      where: { id: userId },
-    });
+  createJwtPayload(userId: number): JwtPayload {
+    return { userId };
+  }
 
-    if (!user) {
-      throw new NotFoundException(`User with id ${userId} not found.`);
-    }
-
-    return user;
+  generateAccessToken(payload: JwtPayload): string {
+    return this.jwtService.sign(payload);
   }
 }
