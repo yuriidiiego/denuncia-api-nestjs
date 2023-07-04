24b539a8f276a94b3ac97986405a588d024bf6b1
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-04 14:20:43 -0300

    feat:🔒 add and comment out LocalStrategy
    - Commented out the LocalStrategy class.
    - Imported the required dependencies.
    - Commented out the inheritance of PassportStrategy for passport-local authentication.
    - Commented out the implementation of the validate method to call AuthService's validateUserCredentials and handle unauthorized exceptions.

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,24 @@
+// import { Injectable, UnauthorizedException } from '@nestjs/common';
+// import { PassportStrategy } from '@nestjs/passport';
+// import { User } from '@prisma/client';
+// import { Strategy } from 'passport-local';
+// import { AuthService } from '../auth.service';
+
+
+// @Injectable()
+// export class LocalStrategy extends PassportStrategy(Strategy) {
+//   constructor(private readonly authService: AuthService) {
+//     super();
+//   }
+
+//   async validate(email: string, password: string): Promise<User> {
+//     const user = await this.authService.validateUserCredentials(
+//       email,
+//       password,
+//     );
+//     if (!user) {
+//       throw new UnauthorizedException();
+//     }
+//     return user;
+//   }
+// }
