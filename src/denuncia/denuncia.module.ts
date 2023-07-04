4b0a0e2e470bb2c5a97283874da5c27034655ea3
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-04 14:01:03 -0300

    refactor: ♻️ reorder Imports in DenunciaModule
    - Reordered the import statements in the DenunciaModule class.
    - Organized the imports for better readability.

--- src/denuncia/denuncia.module.ts
+++ src/denuncia/denuncia.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MapQuestModule } from '../external/mapquest/mapquest.module';
-import { PrismaModule } from '../prisma/prisma.module';
+
 import { DenunciaController } from './denuncia.controller';
 import { DenunciaMapper } from './denuncia.mapper';
 import { DenunciaService } from './denuncia.service';
+import { MapQuestModule } from '../external/mapquest/mapquest.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule, MapQuestModule],
   controllers: [DenunciaController],
-  providers: [DenunciaService, DenunciaMapper],
+  providers: [DenunciaService, DenunciaMapper,],
 })
 export class DenunciaModule {}
