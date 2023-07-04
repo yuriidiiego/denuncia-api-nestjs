c1e0e5ca2fc16e8a208aa74fbd1de3c9178efcbc
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-04 13:56:38 -0300

    refactor: ♻️ refactor DenunciaRequest class
    - Added validation messages for each field.
    - Formatted code for improved readability.

--- src/denuncia/payload/request/create-denuncia.request.ts
+++ src/denuncia/payload/request/create-denuncia.request.ts
@@ -1,58 +1,62 @@
 import { Type } from 'class-transformer';
 import {
-  IsNotEmpty,
-  IsNumber,
-  IsString,
-  Max,
-  Min,
-  ValidateNested,
+IsNotEmpty,
+IsNumber,
+IsString,
+Max,
+Min,
+ValidateNested,
 } from 'class-validator';
 import { DenuncianteRequest } from './create-denunciante.request';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class DenunciaRequest {
-  @ApiProperty({
-    example: 'Título da denúncia',
-    description: 'O título da denúncia',
-    type: String,
-  })
-  @IsNotEmpty()
-  @IsString()
-  titulo: string;
+@ApiProperty({
+example: 'Título da denúncia',
+description: 'O título da denúncia',
+type: String,
+})
+@IsNotEmpty({ message: 'O campo "titulo" não pode estar vazio.' })
+@IsString({ message: 'O campo "titulo" deve ser uma string.' })
+titulo: string;
 
-  @ApiProperty({
-    example: 'Descrição da denúncia',
-    description: 'A descrição da denúncia',
-    type: String,
-  })
-  @IsNotEmpty()
-  @IsString()
-  descricao: string;
+@ApiProperty({
+example: 'Descrição da denúncia',
+description: 'A descrição da denúncia',
+type: String,
+})
+@IsNotEmpty({ message: 'O campo "descricao" não pode estar vazio.' })
+@IsString({ message: 'O campo "descricao" deve ser uma string.' })
+descricao: string;
 
-  @ApiProperty({
-    example: 40.7128,
-    description: 'A latitude da denúncia',
-    type: Number,
-  })
-  @IsNotEmpty()
-  @IsNumber()
-  @Min(-90.0)
-  @Max(90.0)
-  latitude: number;
+@ApiProperty({
+example: 40.7128,
+description: 'A latitude da denúncia',
+type: Number,
+})
+@IsNotEmpty({ message: 'O campo "latitude" não pode estar vazio.' })
+@IsNumber({}, { message: 'O campo "latitude" deve ser um número.' })
+@Min(-90.0, { message: 'O campo "latitude" deve ser maior ou igual a -90.0.' })
+@Max(90.0, { message: 'O campo "latitude" deve ser menor ou igual a 90.0.' })
+latitude: number;
 
-  @ApiProperty({ example: -74.006, description: 'A longitude da denúncia', type: Number })
-  @IsNotEmpty()
-  @IsNumber()
-  @Min(-180.0)
-  @Max(180.0)
-  longitude: number;
+@ApiProperty({
+example: -74.006,
+description: 'A longitude da denúncia',
+type: Number,
+})
+@IsNotEmpty({ message: 'O campo "longitude" não pode estar vazio.' })
+@IsNumber({}, { message: 'O campo "longitude" deve ser um número.' })
+@Min(-180.0, { message: 'O campo "longitude" deve ser maior ou igual a -180.0.' })
+@Max(180.0, { message: 'O campo "longitude" deve ser menor ou igual a 180.0.' })
+longitude: number;
 
-  @ApiProperty({
-    type: DenuncianteRequest,
-    description: 'As informações do denunciante',
-  })
-  @IsNotEmpty()
-  @ValidateNested()
-  @Type(() => DenuncianteRequest)
-  denunciante: DenuncianteRequest;
+@ApiProperty({
+type: DenuncianteRequest,
+description: 'As informações do denunciante',
+})
+@IsNotEmpty({ message: 'O campo "denunciante" não pode estar vazio.' })
+@ValidateNested()
+@Type(() => DenuncianteRequest)
+denunciante: DenuncianteRequest;
 }
