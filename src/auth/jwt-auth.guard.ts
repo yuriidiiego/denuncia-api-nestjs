d521872a5cbcc44d0b71467367be60537817d59a
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-04 20:45:42 -0300

    refactor: ♻️ enhance JwtAuthGuard
    - Updated the JwtAuthGuard class to override the handleRequest method.
    - Added error handling and UnauthorizedException throwing logic.
    - If an error or no user is provided, an UnauthorizedException is thrown.
    - The exception message is set to "Unauthorized access! Please log in to continue."
    - The handleRequest method now returns the user if no error occurred.

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,5 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest<TUser = any>(err: any, user: any): TUser {
+    if (err || !user) {
+      throw (
+        err ||
+        new UnauthorizedException(
+          'Acesso não autorizado! Faça o login para continuar.',
+        )
+      );
+    }
+    return user;
+  }
+}
