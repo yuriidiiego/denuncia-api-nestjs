2d11d3da1fc055e6efb56a9befff6bb7be2dacb4
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-03 21:12:25 -0300

    refactor: ♻️ update AppModule imports
    - Add AuthModule and UserModule to the imports list in AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { DenunciaModule } from './denuncia/denuncia.module';
 import { MapQuestModule } from './external/mapquest/mapquest.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -14,6 +16,8 @@ import { PrismaModule } from './prisma/prisma.module';
     DenunciaModule,
     PrismaModule,
     MapQuestModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [],
   providers: [
