fd4128ad2b7f4ab3c1f7ba55ece82125a6f71075
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-04 14:05:51 -0300

    refactor: ✨ update NestJS bootstrap
    - Added the `useContainer` function from class-validator to configure NestJS to use the container for dependency injection.
    - The `useContainer` function is called with the selected `AppModule` and the `fallbackOnErrors` option set to true.
    - This ensures that class-validator uses the NestJS container for resolving dependencies and managing scoped instances.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { setupSwagger } from '../swagger.config';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalFilters(new HttpExceptionFilter());
   setupSwagger(app);
 
