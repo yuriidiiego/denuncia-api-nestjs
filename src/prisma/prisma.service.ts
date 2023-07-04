7f846c314bc028c899a62dbfa352aae970b18756
Yuri Nascimento <yuriidiiego@gmail.com>
2023-07-03 21:04:25 -0300

    refactor: ♻️ update PrismaService class
    - Updated the `PrismaService` class to implement the `OnModuleInit` and `OnModuleDestroy` interfaces.
    - Removed the `ConfigService` dependency from the constructor.
    - Added `onModuleInit` method that connects to the database using `$connect` method.
    - Added `onModuleDestroy` method that disconnects from the database using `$disconnect` method.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,20 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  constructor(config: ConfigService) {
-    super({
-      datasources: {
-        db: {
-          url: config.get('DATABASE_URL'),
-        },
-      },
-    });
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
